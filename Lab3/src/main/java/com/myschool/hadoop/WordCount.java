680c3625e0b2514e799f91aafbc83e58c6bd6c63
Chen <657063617@qq.com>
2019-04-23 05:40:46 -0700

    7th commit

--- Lab3/src/main/java/com/myschool/hadoop/WordCount.java
+++ Lab3/src/main/java/com/myschool/hadoop/WordCount.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
 public class WordCount {
     
